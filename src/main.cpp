5c3b96cf36ba208af7d2884d120e872c1b1e685b
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-12-28 20:24:46 -0500

    Create window using RayLib

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+#include "raylib.h"
+
 int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "C++ Software Renderer");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+
+    CloseWindow();
     return 0;
 }
